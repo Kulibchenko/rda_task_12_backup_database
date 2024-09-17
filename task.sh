d4e1743c96ec861f5469e868c6e26c05d832b2ea
Kulib <kulibchenkovalera@gmail.com>
2024-09-18 01:35:21 +0300

    firts commit

--- task.sh
+++ task.sh
@@ -1,1 +1,9 @@
-#! /bin/bash
+#!/bin/bash
+
+mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --result-file=ShopDB_backup.sql;
+
+mysql -u $DB_USER -p$DB_PASSWORD  ShopDBReserve < ShopDB_backup.sql;
+
+mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --no-create-db --result-file=ShopDB_no_create.sql;
+
+mysql -u $DB_USER -p$DB_PASSWORD  ShopDBDevelopment < ShopDB_no_create.sql;
