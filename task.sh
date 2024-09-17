240f99f82711ba0ebb28c827c54f0a62cbea909a
Kulib <kulibchenkovalera@gmail.com>
2024-09-18 02:18:31 +0300

    4th commit

--- task.sh
+++ task.sh
@@ -7,6 +7,6 @@ mysqldump -u "$USER" -p"$PASSWORD" ShopDB --result-file=ShopDB_backup.sql;
 # Restory ShopDB to ShopDBReserve
 mysql -u "$USER" -p"$PASSWORD"  ShopDBReserve < ShopDB_backup.sql;
 # Backup data from ShopDB
-mysqldump -u "$USER" -p"$PASSWORD" ShopDB --no-create-db --result-file=ShopDB_no_create.sql;
+mysqldump -u "$USER" -p"$PASSWORD" ShopDB --no-create-info --result-file=ShopDB_no_create.sql;
 # Restore date to ShopDBDevelopment
 mysql -u "$USER" -p"$PASSWORD"  ShopDBDevelopment < ShopDB_no_create.sql;
